ce391d4e330e95226cac5754c2eaa54f823b61c1
Lucas Pereira <lucasdasilvapereira021@gmail.com>
2025-07-07 12:23:14 -0300

    .

---
+++ variables and data types/strings.py
@@ -0,0 +1,30 @@
+phrase = "Giraffe\nAcademy"
+
+print(phrase)
+
+food = "Pizza"
+
+#Concatenation
+print(food + " is delicious")
+
+# FUNCTIONS
+
+print(phrase.upper()) # Converter pra caps lock a frase inteira
+print(phrase.lower()) # Converter pra minúsculas a frase inteira
+print(phrase.isupper()) # Checar se a frase está em caps lock
+print(phrase.islower()) # Checar se a frase está em minúsculas
+print(len(phrase)) # checar quantidade de caracteres na frase
+print(phrase[0]) # acessar o primeiro caractere da frase
+print(phrase.index("G")) # checar onde está a letra na frase
+print(phrase.replace("Giraffe", "Elephant")) # Mudar uma palavra na frase
+
+greeting = "Hello guysss"
+
+print(greeting.upper())
+print(greeting.lower())
+print(greeting.isupper())
+print(greeting.islower())
+print(len(greeting))
+print(greeting[0])
+print(greeting.index("o"))
+print(greeting.replace("guysss", "MY PEOPLEEE"))
--- variables and data types/variables.py
+++ variables and data types/variables.py
@@ -1,2 +1,11 @@
-characters_name = "John"
-characters_age = 35
+characters_name = "Fred"
+characters_age = 55
+is_Male = True
+
+print("There once was a man named " + characters_name)
+print("He was " + characters_age + " years old")
+
+characters_name = "Mayke"
+characters_age = 25
+print("He really liked the name " + characters_name)
+print("But didn't like being " + characters_age)
