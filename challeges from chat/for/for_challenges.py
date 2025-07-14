8dff1f71148649c36d9b1114cc5455fdb39c713e
Lucas Pereira <lucasdasilvapereira021@gmail.com>
2025-07-14 13:05:54 -0300

    .

---
+++ challeges from chat/for/for_challenges.py
@@ -0,0 +1,13 @@
+for each in range(6):
+    print(each)
+
+
+for letras in "Bom dia":
+    print(letras)
+
+numeros = [3, 5, 7]
+soma = 0
+
+for numero in numeros:
+    soma += numero
+    print(soma)
---
+++ for_loop/for.py
@@ -0,0 +1,10 @@
+friends = ["Jim", "Karen", "Kevin"]
+
+for friend in friends:
+    print(friend)
+
+for index in range(10):
+    print(index)
+
+for name in range(len(friends)):
+    print(friends[name])   
